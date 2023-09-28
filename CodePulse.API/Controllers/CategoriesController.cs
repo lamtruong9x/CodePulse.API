cd028b4241c4ce66fee0515ed682cdb73346fc41
Lam Truong <damlamtruong@gmail.com>
2023-09-28 10:49:30 +0700

    Add project files.

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,67 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await categoryRepository.GetAllAsync();
+            var categoryDtos = new List<CategoryDto>();
+            foreach(var category in categories)
+            {
+                var categoryDto = new CategoryDto()
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                    UrlHandle = category.UrlHandle,
+                };
+
+                categoryDtos.Add(categoryDto);
+            }
+
+            return Ok(categoryDtos);
+        }
+
+       [HttpPost]
+       public async Task<IActionResult> Create([FromBody] CreateCategoryRequestDto request)
+       {
+       // Map dto to domain
+            var category = new Category()
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+           
+
+            category = await categoryRepository.CreateAsync(category);
+
+            var categoryDto = new CategoryDto()
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(categoryDto);
+       } 
+      
+    }
+}
---
+++ CodePulse.API/Models/Domain/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿namespace CodePulse.API.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string  Content { get; set; }
+        public string FeatureImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PulishedDate { get; set; }
+        public string Author { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
